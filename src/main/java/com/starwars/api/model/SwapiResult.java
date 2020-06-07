478818bb3aa64e4a8510f3eaad27f47fb2988d0a
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-06-07 15:11:41 -0300

    better naming

--- src/main/java/com/starwars/api/model/SwapiResult.java
+++ src/main/java/com/starwars/api/model/SwapiResult.java
@@ -8,21 +8,21 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SwapiResult {
     
-    private List<SwapiPlanet> results;
+    private List<SwapiPlanet> planetList;
 
     public SwapiResult() {
 
     }
 
     public SwapiResult(List<SwapiPlanet> results) {
-        this.results = results;
+        this.planetList = results;
     }
 
-    public List<SwapiPlanet> getResults() {
-        return this.results;
+    public List<SwapiPlanet> getPlanetList() {
+        return this.planetList;
     }
 
-    public void setResults(List<SwapiPlanet> results) {
-        this.results = results;
+    public void setResults(List<SwapiPlanet> planets) {
+        this.planetList = planets;
     }
 }
